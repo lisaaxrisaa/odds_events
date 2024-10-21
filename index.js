ef18032bfc8e1003cd515445723cf6912141a8e0
Lisa Fujita <lisafujita90@gmail.com>
2024-10-21 14:43:09 -0400

    DOM manipultation with odd and even numbers

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // TODO: this file! :)
+
+const form = document.querySelector('form');
+const addNumber = document.querySelector('#number');
+const numberBank = document.getElementById('numberBank');
+const sortOne = document.getElementById('sortOne');
+const sortAll = document.getElementById('sortAll');
+const sortOdd = document.querySelector('#odds output');
+const sortEven = document.querySelector('#evens output');
+
+let numsArr = [];
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const number = Number(addNumber.value);
+  if (!isNaN(number)) {
+    numsArr.push(number);
+    numberBank.innerText = numsArr;
+    addNumber.value = '';
+  }
+});
+
+sortOne.addEventListener('click', () => {
+  if (numsArr.length > 0) {
+    const firstNumber = numsArr.shift();
+
+    if (firstNumber % 2 === 0) {
+      sortEven.innerText += firstNumber + '';
+    } else {
+      sortOdd.innerText += firstNumber + '';
+    }
+  }
+  numberBank.innerText = numsArr.join(', ');
+});
+
+sortAll.addEventListener('click', () => {
+  for (let i = 0; i < numsArr.length; i++) {
+    const number = numsArr[i];
+    if (number % 2 === 0) {
+      sortEven.innerText += number + ',';
+    } else {
+      sortOdd.innerText += number + ',';
+    }
+  }
+  numsArr = [];
+  numberBank.innerText = '';
+});
